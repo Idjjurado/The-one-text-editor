53b085972ac1790b9e552887c21b26f33b681092
Ismael Jimenez Jurado <ismael@eduphoria.net>
2023-08-26 20:22:52 -0600

    adding in database.js ToDo code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,45 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('theOneDB', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('theOneDB')) {
+        console.log('theOneDB database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('theOneDB', { keyPath: 'id', autoIncrement: true });
+      console.log('theOneDB database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, textentry) => {
+  console.log('PUT to database');
+  try {
+    const theOnetext = await openDB('theOneDB', 1);
+    const tx = theOnetext.transaction('theOneDB', 'readwrite');
+    const store = tx.objectStore('theOneDB');
+    const request = store.add({ id: id, textentry: textentry });
+    const result = await request;
+    console.log('Your words are now written in the stars', result);
+  } catch (error) {
+    console.error('Error putting data into database:', error);
+  }
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from database');
+  try {
+    const theOnetext = await openDB('theOneDB', 1);
+    const tx = theOnetext.transaction('theOneDB', 'readonly');
+    const store = tx.objectStore('theOneDB');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+  } catch (error) {
+    console.error('Error getting data from database:', error);
+    return [];
+  }
+};
 
 initdb();
